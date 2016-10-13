4e1a383d11f80c045e356cf61f46dad4ea8397c7
Franklin Schrans <f.schrans@me.com>
2016-10-13 18:00:43 +0100

    Move maptools.js

--- backend/maputils/maptools.js
+++ backend/maputils/maptools.js
@@ -1,9 +1,2 @@
-export default class MapTools {
-
-  // Inserts a city in the `city` collection of the database.
-
-  var insertCity = function(city) {
-    let boundingBox = city.boundingBox
-  }
-
-}
+// Test
+// Test2
