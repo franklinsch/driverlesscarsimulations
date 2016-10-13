c9d3145900a895adbe4e37c40bf1ab7571ffe7aa
DANIEL GRUMBERG <dany.grumberg@gmail.com>
2016-10-13 15:54:52 +0100

    Started stubs for framework api

---
+++ framework/framework.py
@@ -0,0 +1,23 @@
+# Get the initial parameters of the simulation
+def getWorld():
+
+# Start communication with the backend presumably through some sort of socket
+def startSimulation():
+
+# Send a new updated world probably bad idea
+def updateWorld(world):
+
+# update specific car route
+def updateRoute(car, route):
+
+# Stop car
+def emergencyStop(car):
+
+# End simulation
+def teardownSimulation(world):
+
+# Get event updated => [Events]
+def eventUpdate():
+# add car
+# delete car
+
