4e044fbcb35ab9adcfdc1959cfa52faa3ce08608
Franklin Schrans <f.schrans@me.com>
2016-10-13 18:46:15 +0100

    Move backend files to backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,17 @@
+// Importing node modules
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+// Importing source files
 const routes = require('./routes/routes');
-const config = require('./config');
-
+// consts
 const app = express();
-
-const db = require('./models/db');
-
-//
-// Register Node.js middleware
-// -----------------------------------------------------------------------------
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use('/', routes);
 
 // arrow functions
-const server = app.listen(config.port, () => {
+const server = app.listen(3000, () => {
+	// destructuring
   const {address, port} = server.address();
-  console.log(`The server is running at http://localhost:${port}/`);
+
+  // string interpolation:
+  console.log(`Example app listening at http://${address}:${port}`);
 });
