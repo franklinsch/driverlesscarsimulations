306974fae8df422cf440d411f4b8d0403156faae
Franklin Schrans <f.schrans@me.com>
2016-10-14 12:18:47 +0100

    Create dummy App component

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+class App extends React.Component {
+  render() {
+    return <div> Hello </div>;
+  }
+}
+
 window.onload = () => {
   ReactDOM.render(<App/>, document.getElementById('main'));
 };
