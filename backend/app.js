d93d48f0f134377e96ed22323706a34d8573ea65
Heiki Riisikamp <hr1514@ic.ac.uk>
2016-10-14 07:58:34 +0100

    Generated project structure and implemented react, webpack, mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './dev/components/App';
 
 window.onload = () => {
   ReactDOM.render(<App/>, document.getElementById('main'));
