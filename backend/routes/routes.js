0127a8503181f70523aa5a995c7dc6aa528a5800
Heiki Riisikamp <hr1514@ic.ac.uk>
2016-10-14 07:58:34 +0100

    Generated project structure and implemented react, webpack, mongodb

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send({ message: "Hello, Franklin!" });
+	res.sendFile('public/index.html', { root: __dirname });
 });
 
 module.exports = router;
