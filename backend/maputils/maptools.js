38f2c3c2fb7c103ed4d5c07d1adbed7dbbc8021d
Franklin Schrans <f.schrans@me.com>
2016-10-15 11:54:11 +0100

    Remove maptools.js

--- backend/maputils/maptools.js
+++
@@ -1,9 +0,0 @@
-export default class MapTools {
-
-  // Inserts a city in the `city` collection of the database.
-
-  var insertCity = function(city) {
-    let boundingBox = city.boundingBox
-  }
-
-}
