fc1323f50eaa8368238ffc211c9a493c39880afd
Franklin Schrans <f.schrans@me.com>
2016-10-15 17:09:29 +0100

    Remove sample code

--- backend/src/routes/home/Home.js
+++ backend/src/routes/home/Home.js
@@ -17,18 +17,7 @@ function Home({ news }) {
     <Layout>
       <div className={s.root}>
         <div className={s.container}>
-          <h1 className={s.title}>React.js News</h1>
-          <ul className={s.news}>
-            {news.map((item, index) => (
-              <li key={index} className={s.newsItem}>
-                <a href={item.link} className={s.newsTitle}>{item.title}</a>
-                <span
-                  className={s.newsDesc}
-                  dangerouslySetInnerHTML={{ __html: item.contentSnippet }}
-                />
-              </li>
-            ))}
-          </ul>
+          <p> Hello </p>
         </div>
       </div>
     </Layout>
