351beab80896e7d81d4c94abe886a7b2100399cb
Heiki Riisikamp <hr1514@ic.ac.uk>
2016-10-16 00:24:06 +0300

    Created React components and parallel watch mode scripts

---
+++ backend/components/App.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react';
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello, world. Again and again!</h1>
+        );
+    }
+}
