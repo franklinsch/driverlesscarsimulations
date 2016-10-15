17f33035168a4541539d8964db63835f3291715a
Heiki Riisikamp <hr1514@ic.ac.uk>
2016-10-16 00:24:06 +0300

    Created React components and parallel watch mode scripts

--- backend/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-window.onload = () => {
-  ReactDOM.render(<App/>, document.getElementById('main'));
-};
--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'app.js'),
+  entry: path.join(__dirname, 'client.js'),
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
