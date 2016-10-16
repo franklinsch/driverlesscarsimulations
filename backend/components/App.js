31573541c8bde137978861fcb3c63953d6f26e0f
Franklin Schrans <f.schrans@me.com>
2016-10-16 16:46:34 +0100

    Set up leaflet

--- backend/components/App.js
+++ backend/components/App.js
@@ -1,14 +1,23 @@
 import React, { PropTypes } from 'react';
-
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 export default class App extends React.Component {
-  static propTypes = {
-
-  };
-
   render() {
+    const position = [51.505, -0.09]
+
+    const style = {
+      height: 300 + 'px',
+      width: 300 + 'px'
+    }
+
     return (
-      <h1>Hello, world. Again and again!</h1>
+      <Map center={position} zoom={13} style={style} >
+      <TileLayer
+      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      </Map>
     );
   }
 }
+
