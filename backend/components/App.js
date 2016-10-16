151d7822051a77a691c71b941a4dd6739e557de2
Franklin Schrans <f.schrans@me.com>
2016-10-16 18:29:58 +0100

    Add text field for testing

--- backend/components/App.js
+++ backend/components/App.js
@@ -2,15 +2,55 @@ import React from 'react';
 import SimulationMap from './SimulationMap/SimulationMap.js';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      coordinateString: "",
+      markers: [[51.505, -0.09]]
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      coordinateString: event.target.value
+    })
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+    const coordinateString = this.state.coordinateString;
+    const coordinate = coordinateString.split(" ").map((str) => {
+      return parseFloat(str); 
+    });
+
+    const markers = this.state.markers.concat([coordinate])
+
+    this.setState({
+      markers: markers
+    })
+  }
+
   render() {
     return (
-      <SimulationMap 
-        width={ 300 + 'px' }
-        height={ 300 + 'px' }
-        position={ [51.505, -0.09] }
-        zoom={ 13 }
-        markers= { [[51.505, -0.09], [51.506, -0.092]] }
-      />
+      <div>
+        <form onSubmit={ (e) => { this.onSubmit(e) } }>
+          <input 
+            type="text" 
+            value={this.state.coordinateString}
+            onChange={(e) => this.onChange(e)}
+          />
+          <input type="submit"/>
+        </form>
+
+        <SimulationMap 
+          width={ 300 + 'px' }
+          height={ 300 + 'px' }
+          position={ [51.505, -0.09] }
+          zoom={ 13 }
+          markers= { this.state.markers }
+        />
+      </div>
     )
   }
 }
