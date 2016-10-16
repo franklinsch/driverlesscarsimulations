b443dda69e47b9719cfa3156e0d4f1288c98e027
Franklin Schrans <f.schrans@me.com>
2016-10-16 17:23:42 +0100

    Refactor SimulationMap

--- backend/components/App.js
+++ backend/components/App.js
@@ -1,23 +1,17 @@
-import React, { PropTypes } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import React from 'react';
+import SimulationMap from './SimulationMap/SimulationMap.js';
 
 export default class App extends React.Component {
   render() {
-    const position = [51.505, -0.09]
-
-    const style = {
-      height: 300 + 'px',
-      width: 300 + 'px'
-    }
-
     return (
-      <Map center={position} zoom={13} style={style} >
-      <TileLayer
-      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      <SimulationMap 
+        width={ 300 + 'px' }
+        height={ 300 + 'px' }
+        position={ [51.505, -0.09] }
+        zoom={ 13 }
+        markers= { [[51.505, -0.09], [51.506, -0.092]] }
       />
-      </Map>
-    );
+    )
   }
 }
 
