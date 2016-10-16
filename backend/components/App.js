a075a1f1888dad5501304c6f6b01dc49863d8000
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2016-10-16 01:15:37 +0100

    Add dummy OpenLayers map to index (WIP)

--- backend/components/App.js
+++ backend/components/App.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-
+import {Map, View, Feature, source, layer, geom} from 'ol-react';
 
 export default class App extends React.Component {
 
@@ -9,7 +9,20 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <h1>Hello, world. Again and again!</h1>
+            <Map view=<View resolution={10000} center={[0, 0]}/>>
+                <layer.Tile>
+                    <source.OSM />
+                </layer.Tile>
+                <layer.Vector>
+                    <source.Vector>
+                        <Feature style={{stroke: {color: [255, 0, 0, 1]}}}>
+                            <geom.LineString>
+                                {[[0, 0], [100000, 0], [100000, 100000], [0, 100000]]}
+                            </geom.LineString>
+                        </Feature>
+                    </source.Vector>
+                </layer.Vector>
+            </Map>
         );
     }
 }
