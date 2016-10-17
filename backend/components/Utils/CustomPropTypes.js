9233fc4b50714254f61c1756d369b993bed0a2c9
Franklin Schrans <f.schrans@me.com>
2016-10-17 16:24:02 +0100

    Add city to CustomPropTypes

--- backend/components/Utils/CustomPropTypes.js
+++ backend/components/Utils/CustomPropTypes.js
@@ -16,4 +16,10 @@ export default class CustomPropTypes {
 
   static car = React.PropTypes.shape(CustomPropTypes._car);
 
+  static _city = {
+    position: CustomPropTypes.position,
+    zoom: React.PropTypes.Number
+  }
+
+  static city = React.PropTypes.shape(CustomPropTypes._city);
 }
