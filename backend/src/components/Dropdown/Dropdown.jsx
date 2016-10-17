e63ce46bf315581501666fb954a14aa1151a68f5
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2016-10-17 02:38:28 +0100

    Add generic Dropdown react component

---
+++ backend/src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+
+export default class Dropdown extends React.Component {
+  static propTypes = {
+    items: React.PropTypes.array,
+    onSelect: React.PropTypes.func
+  }
+
+  _onSelect(itemIndex) {
+    let item = this.props.items[itemIndex].value;
+    return this.props.onSelect(item);
+  }
+
+  render() {
+    let items = [];
+    for (let i = 0; i < this.props.items.length; i++) {
+      items.push(<option value={i}>{item.label}</option>);
+    }
+
+    return <option onSelect={this.props.onSelect}>
+      {items}
+    </option>
+  }
+}
