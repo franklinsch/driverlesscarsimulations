7cd39c4444027e544c2cb7a9cf49640b770fd1f3
Franklin Schrans <f.schrans@me.com>
2016-10-18 11:40:59 +0100

    Disable BS

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,7 +1,6 @@
 // webpack.config.js
 const webpack = require('webpack');
 const path = require('path');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -44,15 +43,6 @@ module.exports = {
       sourcemap: false,
       beautify: false,
       dead_code: true
-    }),
-    new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development,
-      // ./public directory is being served
-      host: 'localhost',
-      port: 3000,
-      server: {
-        baseDir: ['public']
-      }
     })
   ]
 };
