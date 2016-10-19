44c6257b05e2f3dcc165f54005d2e1d63a23cb71
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-10-19 15:01:14 +0100

    Add default route and other constants

--- algorithm/basic.py
+++ algorithm/basic.py
@@ -1,3 +1,11 @@
+TIMESLICE = 1
+CONST_SPEED = 3
+BASE_ROUTE = {'source': [4.78482, 50.68166], 'destination': [4.78482, 50.68166], 'path':
+    [{'start': [4.78482, 50.68166], 'end': [4.78482, 50.68347], 'direction': 1, 'timeLeft': 5, 'totalTime': 5},
+     {'start': [4.78482, 50.68347], 'end': [4.78780, 50.68347], 'direction': 2, 'timeLeft': 5, 'totalTime': 5},
+     {'start': [4.78780, 50.68347], 'end': [4.78780, 50.68166], 'direction': 3, 'timeLeft': 5, 'totalTime': 5},
+     {'start': [4.78780, 50.68166], 'end': [4.78482, 50.68166], 'direction': 4, 'timeLeft': 5, 'totalTime': 5}]}
+
 def add(v1, v2):
   return [v1[0]+v2[0], v1[1]+v2[1]]
 
