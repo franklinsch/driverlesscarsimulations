094232f0ec4d6f7b8e6007e696061d914152be24
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-10-20 15:17:17 +0100

    Make deep copy of base route to allow infinite iterations of path

--- sample_algorithms/one_car.py
+++ sample_algorithms/one_car.py
@@ -4,9 +4,20 @@ sys.path.append('../framework')
 
 import client
 
+from copy import deepcopy
+
 class ConnectionAssistant(client.SAVNConnectionAssistant):
   def handleSimulationStart(self, initialParameters):
-    pass
+    #while True:
+    state = setupCars(1)
+    timestamp = 0
+    for i in range(50):
+      #useApi()
+      state = algo(state)
+      timestamp += TIMESLICE
+      savn.updateCarStates(simulationId, timestamp, translate(state))
+      time.sleep(2)
+    #useApiToEnd()
 
   def handleSimulationDataUpdate(self, update):
     pass
@@ -17,10 +28,10 @@ class ConnectionAssistant(client.SAVNConnectionAssistant):
 TIMESLICE = 1
 CONST_SPEED = 3
 BASE_ROUTE = {'source': [50.68166, 4.78482], 'destination': [50.68166, 4.78482], 'path':
-    [{'start': [50.68166, 4.78482], 'end': [50.68347, 4.78482], 'direction': 1, 'timeLeft': 5, 'totalTime': 5},
-     {'start': [50.68347, 4.78482], 'end': [50.68347, 4.78780], 'direction': 2, 'timeLeft': 5, 'totalTime': 5},
-     {'start': [50.68347, 4.78780], 'end': [50.68166, 4.78780], 'direction': 3, 'timeLeft': 5, 'totalTime': 5},
-     {'start': [50.68166, 4.78780], 'end': [50.68166, 4.78482], 'direction': 4, 'timeLeft': 5, 'totalTime': 5}]}
+    [{'start': [50.68166, 4.78482], 'end': [50.68347, 4.78482], 'direction': 1, 'timeLeft': 2, 'totalTime': 2},
+     {'start': [50.68347, 4.78482], 'end': [50.68347, 4.78780], 'direction': 2, 'timeLeft': 2, 'totalTime': 2},
+     {'start': [50.68347, 4.78780], 'end': [50.68166, 4.78780], 'direction': 3, 'timeLeft': 2, 'totalTime': 2},
+     {'start': [50.68166, 4.78780], 'end': [50.68166, 4.78482], 'direction': 4, 'timeLeft': 2, 'totalTime': 2}]}
 
 def add(v1, v2):
   return [v1[0]+v2[0], v1[1]+v2[1]]
@@ -32,14 +43,12 @@ def scale(v, s):
   return [s*v[0], s*v[1]]
 
 def scheduleNewRoute(car):
-  car['route'] = BASE_ROUTE
+  car['route'] = deepcopy(BASE_ROUTE)
   car['position'] = BASE_ROUTE['source']
 
 def moveCar(car):
   timeLeft = TIMESLICE
   while(timeLeft > 0):
-    print(len(car['route']['path']))
-    print(car['route']['path'][0]['timeLeft'])
     if(car['route']['path'][0]['timeLeft'] <= timeLeft):
       timeLeft -= car['route']['path'][0]['timeLeft']
       del car['route']['path'][0]
@@ -76,29 +85,15 @@ def setupCars(numCars):
 
 def translate(state):
   res = []
-  for car in cars:
+  for car in state:
     res += [{'id': car['id'], 'type': car['type'], 'position': {'lat': car['position'][0], 'lng': car['position'][1]}}]
   return res
 
-def main(argv):
-  if len(argv) != 2:
-    return 1
+if len(sys.argv) != 2:
+  sys_exit(1)
 
-  savn = ConnectionAssistant(int(argv[1]))
-  savn.initSession()
+simulationId = int(sys.argv[1])
+savn = ConnectionAssistant(simulationId)
+savn.initSession()
 
-  #while True:
-  state = setupCars(1)
-  timestamp = 0
-  for i in range(50):
-    #useApi()
-    state = algo(state)
-    timestamp += TIMESLICE
-    savn.updateCarStates(simulationId, timestamp, translate(state))
-    time.sleep(2)
-  #useApiToEnd()
-
-  return 0
-
-if __name__ == '__main__':
-  main(sys.argv) #What about sys_exit?
+sys_exit(0)
