37550bc6dcca2e11947105775a6027e9e4679adf
mak514 <mak514@ic.ac.uk>
2016-10-28 19:44:03 +0100

    Return main page for /simulationID

--- webserver/backend/routes/routes.js
+++ webserver/backend/routes/routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const Simulation = require('../models/Simulation');
+const path = require('path');
 
 router.get('/', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile(path.resolve('public/index.html'));
 });
 
-router.get('/simulations/:simulationID', (req, res) => {
+router.get('/:simulationID', (req, res) => {
   Simulation.findOne({
     _id: req.params.simulationID
     })
     .then((result) => {
-      res.send(result);
+      res.sendFile(path.resolve('public/index.html'));
     })
     .catch((err) => {
       res.send(err);
