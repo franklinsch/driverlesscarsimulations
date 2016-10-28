3445b8c65a8f82eab7c68851f6d48a9e2e3134a1
mak514 <mak514@ic.ac.uk>
2016-10-28 18:04:25 +0100

    Add route for simulations and throw error if invalid simulation

--- webserver/backend/routes/routes.js
+++ webserver/backend/routes/routes.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const Simulation = require('../models/Simulation');
 
 router.get('/', (req, res) => {
-	res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('public/index.html', { root: __dirname });
+});
+
+router.get('/simulations/:simulationID', (req, res) => {
+  Simulation.findOne({
+    _id: req.params.simulationID
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 router.get('/simulations/:simulationID', (req, res) => {
