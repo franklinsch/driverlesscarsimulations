6b6af2fe1d272d0b8b26f55857b59a6866c9f956
Franklin Schrans <f.schrans@me.com>
2016-10-29 16:48:17 +0100

    Implement CSV file reading

--- webserver/frontend/components/Main/SimulationSettings/JourneySettings/JourneyImport/JourneyImport.js
+++ webserver/frontend/components/Main/SimulationSettings/JourneySettings/JourneyImport/JourneyImport.js
@@ -2,10 +2,74 @@ import React from 'react';
 
 export default class JourneyImport extends React.Component {
 
+  static propTypes = {
+    handleJourneysSubmit: React.PropTypes.func.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      file: null
+    }
+  }
+
+  _toJourneys(csv) {
+    const journeyTexts = csv.split(/\r?\n/);
+    
+    const journeys = journeyTexts.map((journeyText) => {
+      const {originLat, originLng, destLat, destLng} = journeyText.split(",");
+
+      return {
+        origin: {
+          lat: originLat,
+          lng: originLng
+        }, 
+        destination: {
+          lat: destLat,
+          lng: destLng
+        }
+      }
+    })
+
+    this.props.handleJourneysSubmit(journeys);
+  }
+
+  _handleFileChange(e) {
+    const file = e.target.files[0];
+
+    if (!file || file.type !== "text/csv") {
+      this.setState({
+        file: null
+      });
+
+      console.error("Inputted file is not a CSV");
+      return
+    }
+
+    this.setState({
+      file: e.target.files[0]
+    })
+  }
+
+  _handleFileSubmit(e) {
+    e.preventDefault();
+
+    const file = this.state.file;
+    const reader = new FileReader(); 
+
+    reader.onloadend = () => {
+      this._toJourneys(reader.result);
+    }
+
+    reader.readAsText(file);
+  }
+
   render() {
     return (
       <form>
-        <input type="file"/>
+        <input type="file" accept="text/csv" onChange={(e) => this._handleFileChange(e)}/>
+        <button type="submit" onClick={(e) => this._handleFileSubmit(e)}> Import Journeys </button>
       </form>
     )
   }
