f62eebb1af0f3b20280bdbc8f4db5c928e702598
Franklin Schrans <f.schrans@me.com>
2016-10-29 17:07:13 +0100

    Use JSON instead of CSV

--- webserver/frontend/components/Main/SimulationSettings/JourneySettings/JourneyImport/JourneyImport.js
+++ webserver/frontend/components/Main/SimulationSettings/JourneySettings/JourneyImport/JourneyImport.js
@@ -14,36 +14,20 @@ export default class JourneyImport extends React.Component {
     }
   }
 
-  _toJourneys(csv) {
-    const journeyTexts = csv.split(/\r?\n/);
-    
-    const journeys = journeyTexts.map((journeyText) => {
-      const journeyArray = journeyText.split(",");
-
-      return {
-        origin: {
-          lat: journeyArray[0],
-          lng: journeyArray[1]
-        }, 
-        destination: {
-          lat: journeyArray[2],
-          lng: journeyArray[3]
-        }
-      }
-    })
-
+  _toJourneys(json) {
+    const journeys = JSON.parse(json);
     this.props.handleJourneysSubmit(journeys);
   }
 
   _handleFileChange(e) {
     const file = e.target.files[0];
 
-    if (!file || file.type !== "text/csv") {
+    if (!file || file.type !== "application/json") {
       this.setState({
         file: null
       });
 
-      console.error("Inputted file is not a CSV");
+      console.error("Inputted file is not a JSON");
       return
     }
 
@@ -68,7 +52,7 @@ export default class JourneyImport extends React.Component {
   render() {
     return (
       <form>
-        <input type="file" accept="text/csv" onChange={(e) => this._handleFileChange(e)}/>
+        <input type="file" accept="application/json" onChange={(e) => this._handleFileChange(e)}/>
         <button type="submit" onClick={(e) => this._handleFileSubmit(e)}> Import Journeys </button>
       </form>
     )
