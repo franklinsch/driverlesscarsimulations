ac635de4b185dc252c0f3f376ee8bbdc8f3a33a0
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-10-29 12:49:58 +0100

    Fix addition of duplicate points in geojson

--- sample_algorithms/find_node.js
+++ sample_algorithms/find_node.js
@@ -1,5 +1,5 @@
-point = JSON.parse(process.argv[2].substring(1))['geometry']['coordinates'];
-geojson = JSON.parse(process.argv[3].substring(1));
+const point = JSON.parse(process.argv[2].substring(1))['geometry']['coordinates'];
+const geojson = JSON.parse(process.argv[3].substring(1));
 
 distance = function(point1, point2) {
   return Math.sqrt(Math.pow(point1[0]-point2[0], 2) + Math.pow(point1[1]-point2[1], 2));
@@ -23,6 +23,10 @@ add = function(point1, point2) {
 
 scale = function(point, scale) {
   return [scale * point[0], scale * point[1]];
+};
+
+equal = function(point1, point2) {
+  return point1[0] == point2[0] && point1[1] == point2[1];
 };
 
 var min2_angle = undefined, min2_point, min2_i, min2_j;
@@ -64,14 +68,14 @@ for (let i in geojson['features']) {
   }
 }
 
-B = sub(min2_point, min_point);
-A = sub(point, min_point);
-proj = scale(B, dot(A, B)/dot(B, B));
-proj = add(proj, min_point);
+const B = sub(min2_point, min_point);
+const A = sub(point, min_point);
+const sc = scale(B, dot(A, B)/dot(B, B));
+const proj = add(sc, min_point);
 if (min2_i == min_i && Math.abs(min2_j - min_j) == 1) {
   console.log(proj);
 
-  if (min_distance != 0) {
+  if (min_distance != 0 && !equal(proj, min_point)) {
     j = min_j;
     if (min2_j > min_j) {
       j = min2_j;
