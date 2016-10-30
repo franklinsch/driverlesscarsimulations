c5bf5db5ca14fcb1a9e01a6cc8fc8ddd37dad797
Heiki Riisikamp <hr1514@ic.ac.uk>
2016-10-30 12:20:56 +0000

    Add route to add car journeys in bulk

--- webserver/backend/routes/routes.js
+++ webserver/backend/routes/routes.js
@@ -18,4 +18,31 @@ router.get('/simulations/:simulationID', (req, res) => {
 	});
 });
 
+router.route('/simulations/:simulationID/journeys')
+  .get((req, res) => {
+    Simulation.findOne({
+  		_id: req.params.simulationID
+  	})
+  	.then((result) => {
+  		res.json(result.simulationStartParameters.journeys);
+  	})
+  	.catch((err) => {
+  		res.send(err);
+  	});
+  })
+  .post((req, res) => {
+    const id = req.params.simulationID;
+    const journeys = req.body.journeys;
+    const updateInfo = {
+      $push: { journeys: journeys }
+    };
+    Simulation.update({ _id: id }, updateInfo)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+  });
+
 module.exports = router;
