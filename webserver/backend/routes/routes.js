9928ac885cfbe8792921f16a67e8df4f3e062c15
mak514 <mak514@ic.ac.uk>
2016-10-31 12:23:34 +0000

    Revert "Add route for simulations and throw error if invalid simulation"
    This reverts commit 37f79e94857729343d1d5c3c13367cce15fccae9.

--- webserver/backend/routes/routes.js
+++ webserver/backend/routes/routes.js
@@ -3,19 +3,7 @@ const router = express.Router();
 const Simulation = require('../models/Simulation');
 
 router.get('/', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname });
-});
-
-router.get('/simulations/:simulationID', (req, res) => {
-  Simulation.findOne({
-    _id: req.params.simulationID
-    })
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
+	res.sendFile('public/index.html', { root: __dirname });
 });
 
 router.get('/simulations/:simulationID', (req, res) => {
