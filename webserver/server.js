65aabbfa04c87378458d3a1968a8becefda50ded
mak514 <mak514@ic.ac.uk>
2016-11-02 19:28:10 +0000

    Add more missing breaks

--- webserver/server.js
+++ webserver/server.js
@@ -251,8 +251,10 @@ frameworkSocketServer.on('request', function(request) {
       switch(messageData.type) {
       case "simulation-start":
         _handleSimulationStart(messageData);
+        break;
       case "simulation-state":
         _handleSimulationStateUpdate(messageData);
+        break;
       }
     }
     else if (message.type === 'binary') {
