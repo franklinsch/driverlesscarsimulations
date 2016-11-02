60c1c339d507207ab0b4cbc3881bdadc767494e8
mak514 <mak514@ic.ac.uk>
2016-11-02 18:26:45 +0000

    Change relative paths in index.html to absolute paths

--- webserver/backend/routes/routes.js
+++ webserver/backend/routes/routes.js
@@ -3,24 +3,11 @@ const router = express.Router();
 const Simulation = require('../models/Simulation');
 const path = require('path');
 
-router.get('/simulation/:simulationid', (req, res) => {
+router.get('/simulations/:simulationid', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 })
 
 router.get('*', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 });
-
-router.get('/simulations/:simulationID', (req, res) => {
-	Simulation.findOne({
-		_id: req.params.simulationID
-	})
-	.then((result) => {
-		res.send(result);
-	})
-	.catch((err) => {
-		res.send(err);
-	});
-});
-
 module.exports = router;
