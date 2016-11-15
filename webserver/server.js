6cad2f5273c456c94cc3e3b2337af751ae354ef2
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-11-15 13:29:26 +0000

    Allow server to receive request-simulation-timestamp-change events, and send latestTimestamp with states to frontend

--- webserver/server.js
+++ webserver/server.js
@@ -217,6 +217,31 @@ frontendSocketServer.on('request', function(request) {
     frontendInfo[index]['speed'] = message.content.simulationSpeed;
   }
 
+  function _handleRequestSimulationTimestampChange(message) {
+    let index = frontendConnections.indexOf(connection);
+    Simulation.findById(message.content.simulationID, function (error, simulation) {
+      if (error || !simulation) {
+        connection.send(JSON.stringify({
+          type: "simulation-error",
+          content: {
+            message: "Could not find simulation with ID " + message.content.simulationID
+          }
+        }));
+        console.log("Could not find simulation with ID " + message.content.simulationID);
+        return
+      }
+
+      frontendInfo[index]['timestamp'] = message.content.timestamp;
+      const stateIndex = Math.floor(frontendInfo[index]['timestamp'] / simulation.timeslice);
+      frontendConnections[index].send(JSON.stringify({
+        type: "simulation-state",
+        content: {
+          state: simulation.simulationStates[stateIndex]
+        }
+      }))
+    });
+  }
+
   function _handleRequestSimulationClose(message) {
     Simulation.findById(message.content.simulationID, function (error, simulation) {
       if (error || !simulation) {
@@ -271,6 +296,9 @@ frontendSocketServer.on('request', function(request) {
         break;
       case "request-simulation-speed-change":
         _handleRequestSimulationSpeedChange(messageData);
+        break;
+      case "request-simulation-timestamp-change":
+        _handleRequestSimulationTimestampChange(messageData);
         break;
       case "request-simulation-close":
         _handleRequestSimulationClose(messageData);
@@ -372,7 +400,10 @@ frameworkSocketServer.on('request', function(request) {
         const stateIndex = Math.floor(frontendInfo[index]['timestamp'] / simulation.timeslice);
         frontendConnections[index].send(JSON.stringify({
           type: "simulation-state",
-          content: simulation.simulationStates[stateIndex]
+          content: {
+            state: simulation.simulationStates[stateIndex],
+            latestTimestamp: message.content.timestamp
+          }
         }))
       }
     });
