a75a72e1aeae9a6ce2a93ee30f3d1aa5ff4114b6
mak514 <mak514@ic.ac.uk>
2016-11-18 01:32:30 +0000

    Change log file path

--- stresstest/plots/chart.js
+++ stresstest/plots/chart.js
@@ -3,9 +3,8 @@ google.charts.setOnLoadCallback(drawGraphs);
 
 function drawGraphs() {
   var experimentData;
-
   $.ajax({
-    url:'data.json',
+    url:'example.json',
     dataType: 'json',
     async: false,
     success: function(data) {
@@ -20,9 +19,11 @@ function drawGraphs() {
 }
 
 function drawDbTimeAgainstCarNumbers(experimentData) {
+  
   var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Car Numbers');
+  data.addColumn('number', 'X');
   data.addColumn('number', 'dbTime');
+
 
   var rowData = [];
   for (i = 0; i < experimentData.length; i++) {
@@ -40,19 +41,25 @@ function drawDbTimeAgainstCarNumbers(experimentData) {
   };
 
   var chart = new google.charts.Line(document.getElementById('dbtime_car_chart_div'));
+
   chart.draw(data, options);
 }
 
 function drawDbTimeAgainstTimestamp(experimentData) {
+
+
+
   var data = new google.visualization.DataTable();
-  data.addColumn('number', 'timestamp');
+  data.addColumn('number', 'X');
   data.addColumn('number', 'dbTime');
+
 
   var rowData = [];
   for (i = 0; i < experimentData.length; i++) {
     rowData.push([experimentData[i].timestamp, experimentData[i].dbTime]);
   }
   data.addRows(rowData);
+
 
   var options = {
     hAxis: {
@@ -63,14 +70,16 @@ function drawDbTimeAgainstTimestamp(experimentData) {
     height: 500
   };
 
-  var chart = new google.charts.Line(document.getElementById('dbtime_timestamp_chart_div'));
+   var chart = new google.charts.Line(document.getElementById('dbtime_timestamp_chart_div'));
+
   chart.draw(data, options);
 }
-
 function drawNetworkTimeAgainstCarNumbers(experimentData) {
+
   var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Car Numbers');
+  data.addColumn('number', 'X');
   data.addColumn('number', 'networkTime');
+
 
   var rowData = [];
   for (i = 0; i < experimentData.length; i++) {
@@ -92,15 +101,20 @@ function drawNetworkTimeAgainstCarNumbers(experimentData) {
 }
 
 function drawNetworkTimeAgainstTimestamp(experimentData) {
+
+
+
   var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Timestamp');
+  data.addColumn('number', 'X');
   data.addColumn('number', 'networkTime');
+
 
   var rowData = [];
   for (i = 0; i < experimentData.length; i++) {
     rowData.push([experimentData[i].timestamp, experimentData[i].networkTime]);
   }
   data.addRows(rowData);
+
 
   var options = {
     hAxis: {
