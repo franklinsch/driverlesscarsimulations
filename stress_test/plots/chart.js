5b44de1ed9836ffb9cf7b28d7b0513cf1a67da4b
mak514 <mak514@ic.ac.uk>
2016-11-18 01:28:31 +0000

    Draw plots based on Json data

--- stress_test/plots/chart.js
+++ stress_test/plots/chart.js
@@ -1,55 +1,130 @@
 google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawCrosshairs);
-$(document).ready(function () {
-})
-function drawCrosshairs() {
+google.charts.setOnLoadCallback(drawGraphs);
 
+function drawGraphs() {
+  var experimentData;
   $.ajax({
     url:'example.json',
     dataType: 'json',
     async: false,
     success: function(data) {
-      console.log(data)
+      experimentData = data;
     }
   });
 
+  drawDbTimeAgainstCarNumbers(experimentData);
+  drawDbTimeAgainstTimestamp(experimentData);
+  drawNetworkTimeAgainstCarNumbers(experimentData);
+  drawNetworkTimeAgainstTimestamp(experimentData);
+}
+
+function drawDbTimeAgainstCarNumbers(experimentData) {
+  
   var data = new google.visualization.DataTable();
   data.addColumn('number', 'X');
-  data.addColumn('number', 'Cars');
-  data.addColumn('number', 'Cats');
+  data.addColumn('number', 'dbTime');
 
-  data.addRows([
-    [0, 0, 0],    [1, 10, 5],   [2, 23, 15],  [3, 17, 9],   [4, 18, 10],  [5, 9, 5],
-    [6, 11, 3],   [7, 27, 19],  [8, 33, 25],  [9, 40, 32],  [10, 32, 24], [11, 35, 27],
-    [12, 30, 22], [13, 40, 32], [14, 42, 34], [15, 47, 39], [16, 44, 36], [17, 48, 40],
-    [18, 52, 44], [19, 54, 46], [20, 42, 34], [21, 55, 47], [22, 56, 48], [23, 57, 49],
-    [24, 60, 52], [25, 50, 42], [26, 52, 44], [27, 51, 43], [28, 49, 41], [29, 53, 45],
-    [30, 55, 47], [31, 60, 52], [32, 61, 53], [33, 59, 51], [34, 62, 54], [35, 65, 57],
-    [36, 62, 54], [37, 58, 50], [38, 55, 47], [39, 61, 53], [40, 64, 56], [41, 65, 57],
-    [42, 63, 55], [43, 66, 58], [44, 67, 59], [45, 69, 61], [46, 69, 61], [47, 70, 62],
-    [48, 72, 64], [49, 68, 60], [50, 66, 58], [51, 65, 57], [52, 67, 59], [53, 70, 62],
-    [54, 71, 63], [55, 72, 64], [56, 73, 65], [57, 75, 67], [58, 70, 62], [59, 68, 60],
-    [60, 64, 56], [61, 60, 52], [62, 65, 57], [63, 67, 59], [64, 68, 60], [65, 69, 61],
-    [66, 70, 62], [67, 72, 64], [68, 75, 67], [69, 80, 72]
-  ]);
+
+  var rowData = [];
+  for (i = 0; i < experimentData.length; i++) {
+    rowData.push([experimentData[i].numCars, experimentData[i].dbTime]);
+  }
+  data.addRows(rowData);
 
   var options = {
     hAxis: {
-      title: 'networkTime'
-    },
-    vAxis: {
       title: 'CarNumbers'
     },
-    colors: ['#a52714', '#097138'],
-    crosshair: {
-      color: '#000',
-      trigger: 'selection'
-    }
+    colors: ['#a52714'],
+    width: 1000,
+    height: 500
   };
 
-  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  var chart = new google.charts.Line(document.getElementById('dbtime_car_chart_div'));
 
   chart.draw(data, options);
-  chart.setSelection([{row: 38, column: 1}]);
+}
 
+function drawDbTimeAgainstTimestamp(experimentData) {
+
+
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'X');
+  data.addColumn('number', 'dbTime');
+
+
+  var rowData = [];
+  for (i = 0; i < experimentData.length; i++) {
+    rowData.push([experimentData[i].timestamp, experimentData[i].dbTime]);
+  }
+  data.addRows(rowData);
+
+
+  var options = {
+    hAxis: {
+      title: 'Timestamp'
+    },
+    colors: ['#a52714'],
+    width: 1000,
+    height: 500
+  };
+
+   var chart = new google.charts.Line(document.getElementById('dbtime_timestamp_chart_div'));
+
+  chart.draw(data, options);
+}
+function drawNetworkTimeAgainstCarNumbers(experimentData) {
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'X');
+  data.addColumn('number', 'networkTime');
+
+
+  var rowData = [];
+  for (i = 0; i < experimentData.length; i++) {
+    rowData.push([experimentData[i].numCars, experimentData[i].networkTime]);
+  }
+  data.addRows(rowData);
+
+  var options = {
+    hAxis: {
+      title: 'CarNumbers'
+    },
+    colors: ['#a52714'],
+    width: 1000,
+    height: 500
+  };
+
+  var chart = new google.charts.Line(document.getElementById('network-time_car_chart_div'));
+  chart.draw(data, options);
+}
+
+function drawNetworkTimeAgainstTimestamp(experimentData) {
+
+
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'X');
+  data.addColumn('number', 'networkTime');
+
+
+  var rowData = [];
+  for (i = 0; i < experimentData.length; i++) {
+    rowData.push([experimentData[i].timestamp, experimentData[i].networkTime]);
+  }
+  data.addRows(rowData);
+
+
+  var options = {
+    hAxis: {
+      title: 'Timestamp'
+    },
+    colors: ['#a52714'],
+    width: 1000,
+    height: 500
+  };
+
+  var chart = new google.charts.Line(document.getElementById('network-time_timestamp_chart_div'));
+  chart.draw(data, options);
 }
