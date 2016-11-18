3bd7b84a5398b69c4b58572674674d18e3911653
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-11-18 09:55:01 +0000

    Add awaits in the pedestrian algorithm

--- sample_algorithms/pedestrians.py
+++ sample_algorithms/pedestrians.py
@@ -29,6 +29,8 @@ class ConnectionAssistant(client.SAVNConnectionAssistant):
 
   def handleSimulationCommunication(self, data):
     analyseData(data)
+    global await
+    await = False
 
   def handleSimulationStop(self, info):
     pass
@@ -40,12 +42,17 @@ def runSimulation(savn, initialParameters):
 
   print('\tSending data every ' + str(SLEEP_TIME) + ' seconds')
   nextEventTime = generateEventTime()
+
+  global await
+  await = False
   while savn.alive:
-    #useApi()
+    while (await):
+      time.sleep(SLEEP_TIME)
     savn.updateState(timestamp, translate(state))
     state = executePedestrianAlgorithm(state, timestamp)
     timestamp += TIMESLICE
     time.sleep(SLEEP_TIME)
+    await = True
   #useApiToEnd()
 
 def analyseData(data):
