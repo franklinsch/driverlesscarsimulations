732b8985a2b415b20571e580bb06425bdcd5c110
Franklin Schrans <f.schrans@me.com>
2016-11-25 08:15:37 +0000

    Add no-op function

--- webserver/server.js
+++ webserver/server.js
@@ -597,7 +597,7 @@ frontendSocketServer.on('request', function(request) {
           _handleRequestSimulationJoin(messageData);
           break;
         case "request-simulation-update":
-          _handleRequestEventUpdate(messageData);
+          _handleRequestEventUpdate(messageData, () => {});
           break;
         case "request-simulation-speed-change":
           _handleRequestSimulationSpeedChange(messageData);
