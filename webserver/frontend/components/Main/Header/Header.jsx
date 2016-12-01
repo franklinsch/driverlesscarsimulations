7faa33e91132f20f469aabae682b0c585bb1c78f
Franklin Schrans <f.schrans@me.com>
2016-12-01 18:39:09 +0000

    Remove useless constructor

--- webserver/frontend/components/Main/Header/Header.jsx
+++ webserver/frontend/components/Main/Header/Header.jsx
@@ -8,9 +8,6 @@ import UtilFunctions from '../../Utils/UtilFunctions.jsx';
 
 export default class Header extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
   static propTypes = {
     availableCities: React.PropTypes.arrayOf(CustomPropTypes.city),
     token: React.PropTypes.string,
