674070a358aeccbe84fa31eb805fc38ddf3c0810
mak514 <mak514@ic.ac.uk>
2016-12-26 10:47:42 +0000

    Change select to dropdown button

--- webserver/frontend/components/Main/Header/Dropdown/Dropdown.jsx
+++ webserver/frontend/components/Main/Header/Dropdown/Dropdown.jsx
@@ -14,28 +14,45 @@ export default class Dropdown extends React.Component {
   }
 
   componentDidMount() {
-    $('select').material_select();
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      }
+    );
   }
 
   componentDidUpdate() {
-    $('select').material_select();
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      }
+    );
   }
 
   render() {
     let items = [];
     for (let i = 0; i < this.props.items.length; i++) {
       //TODO: Not a generalised Dropdown. Either specific further, or generalise
-      items.push(<option key={i} value={i}>{this.props.items[i].name}</option>);
+      items.push(<li><a href="#!"> {this.props.items[i].name}</a></li>);
     }
     //TODO: Replace with map below
     return (
-			<div className="input-field">
-        <select disabled={!this.props.enabled}
-          onChange={::this._onSelect}
-        >
+      <div>
+        <a className='dropdown-button btn' href='#' data-activates='dropdown1'>Select city</a>
+        <ul id='dropdown1' className='dropdown-content'>
           {items}
-        </select>
-			</div>
-		);
+        </ul>
+      </div>
+    );
   }
 }
