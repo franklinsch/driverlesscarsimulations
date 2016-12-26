506185620a4722a826144972638f9b074c4e0187
mak514 <mak514@ic.ac.uk>
2016-12-26 11:53:53 +0000

    Fix dropdown alignment

--- webserver/frontend/components/Main/Header/Dropdown/Dropdown.jsx
+++ webserver/frontend/components/Main/Header/Dropdown/Dropdown.jsx
@@ -19,10 +19,10 @@ export default class Dropdown extends React.Component {
     $('.dropdown-button').dropdown({
         inDuration: 300,
         outDuration: 225,
-        constrain_width: false, // Does not change width of dropdown to that of the activator
-        hover: true, // Activate on hover
-        gutter: 0, // Spacing from edge
-        belowOrigin: false, // Displays dropdown below the button
+        constrain_width: true, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 15, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
         alignment: 'left' // Displays dropdown with edge aligned to the left of button
       }
     );
@@ -32,10 +32,10 @@ export default class Dropdown extends React.Component {
     $('.dropdown-button').dropdown({
         inDuration: 300,
         outDuration: 225,
-        constrain_width: false, // Does not change width of dropdown to that of the activator
-        hover: true, // Activate on hover
-        gutter: 0, // Spacing from edge
-        belowOrigin: false, // Displays dropdown below the button
+        constrain_width: true, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 15, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
         alignment: 'left' // Displays dropdown with edge aligned to the left of button
       }
     );
@@ -45,13 +45,13 @@ export default class Dropdown extends React.Component {
     let items = [];
     for (let i = 0; i < this.props.items.length; i++) {
       //TODO: Not a generalised Dropdown. Either specific further, or generalise
-      items.push(<li key={i}><a href="#!" name={i} onClick={::this._onSelect}> {this.props.items[i].name}</a></li>);
+      items.push(<li className="z-depth-1" key={i}><a href="#!" name={i} onClick={::this._onSelect}> {this.props.items[i].name}</a></li>);
     }
     //TODO: Replace with map below
     return (
       <div>
-        <a className='dropdown-button btn' href='#' data-activates='dropdown1'>Select city</a>
-        <ul id='dropdown1' className='dropdown-content'>
+        <a className='dropdown-button btn' href='#' data-activates='dropdown-cities'>Select city</a>
+        <ul id='dropdown-cities' className='dropdown-content'>
           {items}
         </ul>
       </div>
