fe79a62f04c1b328d77f872cce9ce32fff63c731
mak514 <mak514@ic.ac.uk>
2016-12-27 12:50:41 +0000

    Add tab icons

--- webserver/frontend/components/Main/Header/Header.jsx
+++ webserver/frontend/components/Main/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import CustomPropTypes from '../../Utils/CustomPropTypes.jsx';
-import Dropdown from './Dropdown/Dropdown.jsx';
-import JoinSimulationForm from './JoinSimulationForm/JoinSimulationForm.jsx';
-import LoginButton from './LoginButton/LoginButton.jsx';
-import SimulationList from './SimulationList/SimulationList.jsx';
-import UtilFunctions from '../../Utils/UtilFunctions.jsx';
+import React from "react";
+import CustomPropTypes from "../../Utils/CustomPropTypes.jsx";
+import Dropdown from "./Dropdown/Dropdown.jsx";
+import JoinSimulationForm from "./JoinSimulationForm/JoinSimulationForm.jsx";
+import LoginButton from "./LoginButton/LoginButton.jsx";
+import SimulationList from "./SimulationList/SimulationList.jsx";
 
 export default class Header extends React.Component {
 
@@ -55,34 +54,60 @@ export default class Header extends React.Component {
     return (
       <div>
         <ul className="side-nav fixed">
-          <a className="brand-logo center" href="#">SAVN</a>
+          <li>
+            <a className="brand-logo center" href="#">SAVN</a>
+          </li>
           <li>
             <div className="row">
+              <div className="col s12">
                 <ul className="tabs">
-                  <li className="tab col s3"><a href="#test1">Test 1</a></li>
-                  <li className="tab col s3"><a class="active" href="#test2">Test 2</a></li>
-                  <li className="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
-                  <li className="tab col s3"><a href="#test4">Test 4</a></li>
+                  <li className="tab col s3">
+                    <a href="#account">
+                      <span className="center-align">
+                        <i className="material-icons">account_box</i>
+                    </span>
+                    </a>
+                  </li>
+                  <li className="tab col s3">
+                    <a href="#settings">
+                      <span className="center-align">
+                        <i className="material-icons">settings</i>
+                    </span>
+                    </a>
+                  </li>
+                  <li className="tab col s3">
+                    <a href="#search">
+                      <span className="center-align">
+                        <i className="material-icons">search</i>
+                    </span>
+                    </a>
+                  </li>
+                  <li className="tab col s3">
+                    <a href="#run">
+                      <span className="center-align">
+                        <i className="material-icons">play_arrow</i>
+                    </span>
+                    </a>
+                  </li>
                 </ul>
               </div>
-              <div id="test1" className="col s12">Test 1</div>
-              <div id="test2" className="col s12">Test 2</div>
-              <div id="test3" className="col s12">Test 3</div>
-              <div id="test4" className="col s12">Test 4</div>
-          </li>
-          <li>
-            <Dropdown
-              enabled  = {this.props.enabled}
-              items    = {cities}
-              handlers = {dropdownHandlers}
-            />
-          </li>
-          <li>
-            <LoginButton
-              token      = {this.props.token}
-              activeUser = {this.props.activeUser}
-              handlers   = {loginButtonHandlers}
-            />
+              <div id="account" className="col s12">
+                <LoginButton
+                  token      = {this.props.token}
+                  activeUser = {this.props.activeUser}
+                  handlers   = {loginButtonHandlers}
+                />
+              </div>
+              <div id="settings" className="col s12">
+                <Dropdown
+                  enabled  = {this.props.enabled}
+                  items    = {cities}
+                  handlers = {dropdownHandlers}
+                />
+              </div>
+              <div id="search" className="col s12">Test 3</div>
+              <div id="run" className="col s12">Test 3</div>
+            </div>
           </li>
           <li>
             {
