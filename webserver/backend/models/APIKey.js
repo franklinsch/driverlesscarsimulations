986cb70498c4f22490e7671bf6623e6503093fba
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2017-01-05 00:22:59 +0100

    Correctly reference simulation in schema

--- webserver/backend/models/APIKey.js
+++ webserver/backend/models/APIKey.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const apiKeySchema = mongoose.Schema({
+    title: String,
     hash: String,
-    simulationID: String,
+    simulationID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Simulation'
+    }
 });
 
 module.exports = mongoose.model('APIKey', apiKeySchema);
