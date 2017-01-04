8202b3b43ba473907b4f1afc7f84b50e80fc9e64
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2017-01-04 13:12:41 +0400

    Use intersection of great-circle paths for better calculation of nearest projection

--- sample_algorithms/route.js
+++ sample_algorithms/route.js
@@ -17,6 +17,79 @@ const getBearing = function(start, end) {
   return toDegrees(Math.atan2(y, x));
 }
 
+const getDistance = function(p1, p2) {
+  const R = 6371e3;
+  const lat1 = toRadians(p1[1]);
+  const lng1 = toRadians(p1[0]);
+  const lat2 = toRadians(p2[1]);
+  const lng2 = toRadians(p2[0]);
+  const dlat = lat2-lat1;
+  const dlng = lng2-lng1;
+
+  const a = Math.sin(dlat/2)**2 +
+            Math.cos(lat1)*Math.cos(lat2)*Math.sin(dlng/2)**2;
+  const c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  return R*c;
+}
+
+const bearingToDirection = function(brng) {
+  return (-brng+90+180)%360-180;
+}
+
+const getIntersectionPoint = function(p1, b1, p2, b2) {
+  const lat1 = toRadians(p1[1]);
+  const lng1 = toRadians(p1[0]);
+  const lat2 = toRadians(p2[1]);
+  const lng2 = toRadians(p2[0]);
+  const theta13 = toRadians(b1);
+  const theta23 = toRadians(b2);
+
+  const dlat = lat2 - lat1;
+  const dlng = lng2 - lng1;
+
+  const delta12 = 2 * Math.asin(Math.sqrt(Math.sin(dlat/2)**2 + Math.cos(lat1)*Math.cos(lat2)*Math.sin(dlng/2)**2));
+  const theta_a = Math.acos((Math.sin(lat2) - Math.sin(lat1)*Math.cos(delta12)) /
+                            (Math.sin(delta12)*Math.cos(lat1)));
+  const theta_b = Math.acos((Math.sin(lat1) - Math.sin(lat2)*Math.cos(delta12)) /
+                            (Math.sin(delta12)*Math.cos(lat2)));
+
+  let theta12, theta21;
+  if (Math.sin(dlng) > 0) {
+    theta12 = theta_a;
+    theta21 = 2 * Math.PI - theta_b;
+  } else {
+    theta12 = 2 * Math.PI - theta_a;
+    theta21 = theta_b;
+  }
+
+  const alpha1 = (theta13 - theta12 + Math.PI) % (2 * Math.PI) - Math.PI;
+  const alpha2 = (theta21 - theta23 + Math.PI) % (2 * Math.PI) - Math.PI;
+
+  const alpha3 = Math.acos(-Math.cos(alpha1)*Math.cos(alpha2) +
+                            Math.sin(alpha1)*Math.sin(alpha2)*Math.cos(delta12));
+  const delta13 = Math.atan2(Math.sin(delta12)*Math.sin(alpha1)*Math.sin(alpha2),
+                             Math.cos(alpha2) + Math.cos(alpha1)*Math.cos(alpha3));
+  const lat3 = Math.asin(Math.sin(lat1)*Math.cos(delta13) +
+                         Math.cos(lat1)*Math.sin(delta13)*Math.cos(theta13));
+  const dlng13 = Math.atan2(Math.sin(theta13)*Math.sin(delta13)*Math.cos(lat1),
+                            Math.cos(delta13) - Math.sin(lat1)*Math.sin(lat3));
+  const lng3 = lng1 + dlng13;
+  return [(toDegrees(lng3)+540)%360-180, toDegrees(lat3)];
+}
+
+const getNormalIntersection = function(v0, v1, p) {
+  const ang = getBearing(v0, v1);
+  const r_ang = (ang + 180 + 180) % 360 - 180;
+  const min_ang = Math.min(ang, r_ang);
+  const max_ang = Math.max(ang, r_ang);
+  const comp = getBearing(v0, p);
+
+  const mul = (comp >= min_ang && comp <= max_ang) ? 1 : -1;
+  const theta = (max_ang + 90 * mul + 540) % 360 - 180;
+  return getIntersectionPoint(v0, ang, p, theta);
+}
+
 const getNearest = function(p, pathFinder) {
   let min_dist = undefined, min_point, min_edge;
   for (const edge of pathFinder._topo.edges) {
@@ -24,11 +97,8 @@ const getNearest = function(p, pathFinder) {
     if (props['highway']) {
       const v0 = pathFinder._topo.vertices[edge[0]];
       const v1 = pathFinder._topo.vertices[edge[1]];
-      let test = false;
-      if (v0[1] == 51.4983735 || v1[1] == 51.4983735 ||
-          v0[1] == 51.4980849 || v1[1] == 51.4980849) {
-        test = true;
-      }
+
+      let proj = getNormalIntersection(v0, v1, p);
       const Ax = v0[0];
       const Ay = v0[1];
       const Bx = v1[0];
@@ -36,55 +106,44 @@ const getNearest = function(p, pathFinder) {
       const x  = p[0];
       const y  = p[1];
 
-      const dy    = By - Ay;
-      const dx    = Bx - Ax;
-      //const ang   = Math.atan2(dy, dx);
-      const ang   = getBearing(v0, v1);
-      const theta = (ang + 90 + 180) % 360 - 180;
-
-      let m = (dy*(Ax - x) - dx*(Ay - y))/(dy*Math.cos(theta) - dx*Math.sin(theta));
-      let proj = [x + m*Math.cos(theta), y + m*Math.sin(theta)];
-      m = Math.abs(m);
-      if (test) {
-        console.error('Test: ' + m);
-        console.error(dy);
-        console.error(dx);
-        console.error(ang);
-        console.error(theta);
-        console.error(p);
-        console.error(v0);
-        console.error(proj);
-        console.error(v1);
-        console.error();
-      }
-
       const eps = pathFinder._precision;
 
-      let minx = -eps, maxx = eps, left_edge, right_edge;
-      if (Ax < Bx) {
-        minx += Ax;
-        maxx += Bx;
+      let min = -eps, max = eps, left_edge, right_edge;
+      if (Ax == Bx) {
+        if (Ay < By) {
+          min += Ay;
+          max += By;
+          left_edge = v0;
+          right_edge = v1;
+        } else {
+          min += By;
+          max += Ay;
+          right_edge = v0;
+          left_edge = v1;
+        }
+      } else if (Ax < Bx) {
+        min += Ax;
+        max += Bx;
         left_edge = v0;
         right_edge = v1;
       } else {
-        minx += Bx;
-        maxx += Ax;
+        min += Bx;
+        max += Ax;
         right_edge = v0;
         left_edge = v1;
       }
-      const midx = (minx + maxx) / 2;
-      if (proj[0] <= midx) {
-        if (proj[0] < minx) {
+      const mid = (min + max) / 2;
+      if (proj[0] <= mid) {
+        if (proj[0] < min) {
           proj = left_edge;
-          m = (x - proj[0])**2 + (y - proj[1])**2;
         }
-      } else if (proj[0] > midx) {
-        if (proj[0] > maxx) {
+      } else if (proj[0] > mid) {
+        if (proj[0] > max) {
           proj = right_edge;
-          m = (x - proj[0])**2 + (y - proj[1])**2;
         }
       }
 
+      const m = getDistance(proj, p);
       if (min_dist == undefined || m < min_dist) {
         min_dist = m;
         min_point = proj;
@@ -93,14 +152,11 @@ const getNearest = function(p, pathFinder) {
     }
   }
 
-  min_dist = Math.sqrt(min_dist);
   return [min_dist, min_point];
 }
 
 point1 = JSON.parse(process.argv[5].substring(1));
 point2 = JSON.parse(process.argv[6].substring(1));
-console.error(point1);
-console.error(point2);
 
 start = JSON.parse(process.argv[2].substring(1));
 end = JSON.parse(process.argv[3].substring(1));
--- sample_algorithms/test.js
+++ sample_algorithms/test.js
@@ -17,49 +17,92 @@ const getBearing = function(start, end) {
   return toDegrees(Math.atan2(y, x));
 }
 
+const getDistance = function(p1, p2) {
+  const R = 6371e3;
+  const lat1 = toRadians(p1[1]);
+  const lng1 = toRadians(p1[0]);
+  const lat2 = toRadians(p2[1]);
+  const lng2 = toRadians(p2[0]);
+  const dlat = lat2-lat1;
+  const dlng = lng2-lng1;
+
+  const a = Math.sin(dlat/2)**2 +
+            Math.cos(lat1)*Math.cos(lat2)*Math.sin(dlng/2)**2;
+  const c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  return R*c;
+}
+
 const bearingToDirection = function(brng) {
   return (-brng+90+180)%360-180;
 }
 
-const getNearest = function(v0, v1, p) {
-  const Ax = v0[0];
-  const Ay = v0[1];
-  const Bx = v1[0];
-  const By = v1[1];
-  const x  = p[0];
-  const y  = p[1];
+const getIntersectionPoint = function(p1, b1, p2, b2) {
+  const lat1 = toRadians(p1[1]);
+  const lng1 = toRadians(p1[0]);
+  const lat2 = toRadians(p2[1]);
+  const lng2 = toRadians(p2[0]);
+  const theta13 = toRadians(b1);
+  const theta23 = toRadians(b2);
 
-  const dy    = By - Ay;
-  const dx    = Bx - Ax;
-  //const ang   = Math.atan2(dy, dx);
-  const ang   = bearingToDirection(getBearing(v0, v1));
-  const theta = toRadians((ang + 90 + 180) % 360 - 180);
+  const dlat = lat2 - lat1;
+  const dlng = lng2 - lng1;
 
-  let m = (dy*(Ax - x) - dx*(Ay - y))/(dy*Math.cos(theta) - dx*Math.sin(theta));
-  if (m == -0.0002955128860732096) {
-    m = 0.0002656041155;
+  const delta12 = 2 * Math.asin(Math.sqrt(Math.sin(dlat/2)**2 + Math.cos(lat1)*Math.cos(lat2)*Math.sin(dlng/2)**2));
+  const theta_a = Math.acos((Math.sin(lat2) - Math.sin(lat1)*Math.cos(delta12)) /
+                            (Math.sin(delta12)*Math.cos(lat1)));
+  const theta_b = Math.acos((Math.sin(lat1) - Math.sin(lat2)*Math.cos(delta12)) /
+                            (Math.sin(delta12)*Math.cos(lat2)));
+
+  let theta12, theta21;
+  if (Math.sin(dlng) > 0) {
+    theta12 = theta_a;
+    theta21 = 2 * Math.PI - theta_b;
+  } else {
+    theta12 = 2 * Math.PI - theta_a;
+    theta21 = theta_b;
   }
-  let proj = [x + m*Math.cos(theta), y + m*Math.sin(theta)];
-  console.log(getBearing(v0, v1));
-  console.log('Test: ' + m);
-  console.log(dy);
-  console.log(dx);
-  console.log(ang);
-  console.log(theta);
-  console.log(p);
-  console.log(v0);
-  console.log(proj);
-  console.log(v1);
-  console.log();
-  console.log();
-  console.log(getBearing(p, proj));
+
+  const alpha1 = (theta13 - theta12 + Math.PI) % (2 * Math.PI) - Math.PI;
+  const alpha2 = (theta21 - theta23 + Math.PI) % (2 * Math.PI) - Math.PI;
+
+  const alpha3 = Math.acos(-Math.cos(alpha1)*Math.cos(alpha2) +
+                            Math.sin(alpha1)*Math.sin(alpha2)*Math.cos(delta12));
+  const delta13 = Math.atan2(Math.sin(delta12)*Math.sin(alpha1)*Math.sin(alpha2),
+                             Math.cos(alpha2) + Math.cos(alpha1)*Math.cos(alpha3));
+  const lat3 = Math.asin(Math.sin(lat1)*Math.cos(delta13) +
+                         Math.cos(lat1)*Math.sin(delta13)*Math.cos(theta13));
+  const dlng13 = Math.atan2(Math.sin(theta13)*Math.sin(delta13)*Math.cos(lat1),
+                            Math.cos(delta13) - Math.sin(lat1)*Math.sin(lat3));
+  const lng3 = lng1 + dlng13;
+  return [(toDegrees(lng3)+540)%360-180, toDegrees(lat3)];
 }
 
-exports.getNearest = getNearest;
-exports.getBearing = getBearing;
-exports.bearingToDirection = bearingToDirection;
+const getNormalIntersection = function(v0, v1, p) {
+  const ang = getBearing(v0, v1);
+  const r_ang = (ang + 180 + 180) % 360 - 180;
+  const min_ang = Math.min(ang, r_ang);
+  const max_ang = Math.max(ang, r_ang);
+  const comp = getBearing(v0, p);
+  console.log(ang);
+
+  const mul = (comp >= min_ang && comp <= max_ang) ? 1 : -1;
+  const theta = (max_ang + 90 * mul + 540) % 360 - 180;
+  const res = getIntersectionPoint(v0, ang, p, theta);
+  console.log(getBearing(p, res));
+  return res;
+}
 
 let a = [ -0.1689102, 51.498379 ];
 let b = [ -0.1685937, 51.4980849 ];
 let s = [ -0.16835329122841358, 51.49825411834319 ];
-getNearest(a, b, s);
+//console.log(getNormalIntersection(a, b, s));
+//
+let o = [ -0.185773316770792, 51.499566545216325 ];
+a = [-0.1858895,51.4998596];
+b = [-0.1859493,51.4992718];
+console.log(getNormalIntersection(a, b, o));
+let p1 = [ -0.18546949425081038, 51.499609510126845 ];
+let p2 = [ -0.18550942493458247, 51.499436580468185 ];
+console.log(getDistance(o, p1));
+console.log(getDistance(o, p2));
