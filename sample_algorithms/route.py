586162aeadb9fc5eabd7ad05f3b10d5a16e269de
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2017-01-06 19:38:42 +0400

    Remove unused functions

--- sample_algorithms/route.py
+++ sample_algorithms/route.py
@@ -1,11 +1,8 @@
 import os
 import overpass
 import geojson as gjson
-import math
 import json
 import subprocess
-
-import time
 
 def saveGeojson(bottom, left, top, right, output):
   api = overpass.API()
@@ -15,36 +12,9 @@ def saveGeojson(bottom, left, top, right, output):
   f = open(output, 'w')
   f.write(gjson.dumps(response))
 
-def isEqualNodes(point1, point2):
-  return point1[0] == point2[0] and point1[1] == point2[1]
-
 def getRoute(inp, start, end):
   os.system("node route.js " + str(gjson.dumps(start).encode('utf8')) + " " + str(gjson.dumps(end).encode('utf8')) + " " + inp + " > path.json");
   path = None
   with open("path.json") as data_file:
     path = json.load(data_file)
   return path
-
-def newPoint(coordinates):
-  return {"geometry": {"type": "Point", "coordinates": coordinates}, "type": "Feature", "properties": {}}
-
-def distance(point1, point2):
-  return math.sqrt(math.pow(point1[0]-point2[0], 2) + math.pow(point1[1]-point2[1], 2))
-
-def angle(origin, point):
-  return math.atan2(point[1]-origin[1], point[0]-origin[0])
-
-def dot(point1, point2):
-  return point1[0]*point2[0] + point1[1]*point2[1]
-
-def sub(point1, point2):
-  return [point1[0]-point2[0], point1[1]-point2[1]]
-
-def add(point1, point2):
-  return [point1[0]+point2[0], point1[1]+point2[1]]
-
-def scale(point, scale):
-  return [scale * point[0], scale * point[1]]
-
-def equal(point1, point2):
-  return point1[0] == point2[0] and point1[1] == point2[1]
