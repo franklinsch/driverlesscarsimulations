1c38c85aa287ddf4143dbce55cb339e015e09fea
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2017-01-09 03:32:33 +0000

    Modify charting code to enable easier modification of graphing data

--- stresstest/plots/chart.js
+++ stresstest/plots/chart.js
@@ -13,148 +13,63 @@ function drawGraphs() {
     }
   });
 
-  drawDbTimeAgainstCarNumbers(experimentData);
-  drawDbTimeAgainstTimestamp(experimentData);
-  drawNetworkTimeAgainstCarNumbers(experimentData);
-  drawNetworkTimeAgainstTimestamp(experimentData);
+  var numCars = {key: 'numCars', descriptor: 'Number of Cars'};
+  var timestamp = {key: 'timestamp', descriptor: 'Timestamp'};
+  var hAxes = [numCars, timestamp];
+
+  var accessTime = {key: 'accessTime', descriptor: 'Access Time'};
+  var saveTime = {key: 'saveTime', descriptor: 'Save Time'};
+  var networkTime = {key: 'networkTime', descriptor: 'Network Time'};
+  var processTime = {key: 'processTime', descriptor: 'Process Time'};
+  var vAxes = [accessTime, saveTime, networkTime, processTime];
+
+  var html = '';
+  for (var hAxis of hAxes) {
+    for (var vAxis of vAxes) {
+      var div_id = hAxis.key + '_' + vAxis.key + '_chart_div';
+      html += '<div id="' + div_id + '"></div></br></br></br>';
+    }
+  }
+  $('#charts').html(html);
+  for (var hAxis of hAxes) {
+    for (var vAxis of vAxes) {
+      drawHAxisAgainstVAxis(experimentData, hAxis, vAxis);
+    }
+  }
 }
 
-function drawDbTimeAgainstCarNumbers(experimentData) {
+function drawHAxisAgainstVAxis(experimentData, hAxis, vAxis) {
   var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Car Numbers');
+  data.addColumn('number', hAxis.descriptor);
   for (i = 0; i < experimentData.length; i++) {
-    data.addColumn('number', 'dbTime' + i);
+    data.addColumn('number', vAxis.descriptor + i);
   }
   var rowData = [];
-  for (i = 0; i < experimentData.length; i++) {
-    for (j = 0; j < experimentData[i].length; j++) {
+  for (var i = 0; i < experimentData.length; i++) {
+    for (var j = 0; j < experimentData[i].length; j++) {
       var row = []
-      row.push(experimentData[i][j].numCars);
-      for (a = 0; a < i; a++) {
+      row.push(experimentData[i][j][hAxis.key]);
+      for (var a = 0; a < i; a++) {
         row.push(null);
       }
-      row.push(experimentData[i][j].dbTime)
-      for (a = i + 1; a < experimentData.length; a++) {
-        row.push(null)
+      row.push(experimentData[i][j][vAxis.key]);
+      for (var a = i + 1; a < experimentData.length; a++) {
+        row.push(null);
       }
-      rowData.push(row)
+      rowData.push(row);
     }
   }
   data.addRows(rowData);
 
   var options = {
     hAxis: {
-      title: 'CarNumbers'
+      title: hAxis.descriptor
     },
     width: 1000,
     height: 500
   };
 
-  var chart = new google.charts.Line(document.getElementById('dbtime_car_chart_div'));
-  chart.draw(data, options);
-}
-
-function drawDbTimeAgainstTimestamp(experimentData) {
-  var data = new google.visualization.DataTable();
-  data.addColumn('number', 'timestamp');
-  for (i = 0; i < experimentData.length; i++) {
-    data.addColumn('number', 'dbTime' + i);
-  }
-  var rowData = [];
-  for (i = 0; i < experimentData.length; i++) {
-    for (j = 0; j < experimentData[i].length; j++) {
-      var row = []
-      row.push(experimentData[i][j].timestamp);
-      for (a = 0; a < i; a++) {
-        row.push(null);
-      }
-      row.push(experimentData[i][j].dbTime)
-      for (a = i + 1; a < experimentData.length; a++) {
-        row.push(null)
-      }
-      rowData.push(row)
-    }
-  }
-  data.addRows(rowData);
-
-  var options = {
-    hAxis: {
-      title: 'Timestamp'
-    },
-    width: 1000,
-    height: 500
-  };
-
-  var chart = new google.charts.Line(document.getElementById('dbtime_timestamp_chart_div'));
-  chart.draw(data, options);
-}
-
-function drawNetworkTimeAgainstCarNumbers(experimentData) {
-  var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Car Numbers');
-  for (i = 0; i < experimentData.length; i++) {
-    data.addColumn('number', 'networkTime' + i);
-  }
-  var rowData = [];
-  for (i = 0; i < experimentData.length; i++) {
-    for (j = 0; j < experimentData[i].length; j++) {
-      var row = []
-      row.push(experimentData[i][j].numCars);
-      for (a = 0; a < i; a++) {
-        row.push(null);
-      }
-      row.push(experimentData[i][j].networkTime)
-      for (a = i + 1; a < experimentData.length; a++) {
-        row.push(null)
-      }
-      rowData.push(row)
-    }
-  }
-  data.addRows(rowData);
-
-  var options = {
-    hAxis: {
-      title: 'CarNumbers'
-    },
-    width: 1000,
-    height: 500
-  };
-
-  var chart = new google.charts.Line(document.getElementById('network-time_car_chart_div'));
-  chart.draw(data, options);
-}
-
-function drawNetworkTimeAgainstTimestamp(experimentData) {
-  var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Timestamp');
-  for (i = 0; i < experimentData.length; i++) {
-    data.addColumn('number', 'networkTime' + i);
-  }
-  var rowData = [];
-  for (i = 0; i < experimentData.length; i++) {
-    for (j = 0; j < experimentData[i].length; j++) {
-      var row = []
-      row.push(experimentData[i][j].timestamp);
-      for (a = 0; a < i; a++) {
-        row.push(null);
-      }
-      row.push(experimentData[i][j].networkTime)
-      for (a = i + 1; a < experimentData.length; a++) {
-        row.push(null)
-      }
-      rowData.push(row)
-    }
-  }
-  data.addRows(rowData);
-
-  var options = {
-    hAxis: {
-      title: 'Timestamp'
-    },
-    width: 1000,
-    height: 500
-  };
-
-  var chart = new google.charts.Line(document.getElementById('network-time_timestamp_chart_div'));
+  var div_name = hAxis.key + '_' + vAxis.key + '_chart_div';
+  var chart = new google.charts.Line(document.getElementById(div_name));
   chart.draw(data, options);
 }
