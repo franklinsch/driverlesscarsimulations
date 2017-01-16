842ba5202f541bc0ae4bdce3a55f2266db5193b7
Franklin Schrans <f.schrans@me.com>
2017-01-16 15:59:10 +0000

    Add more tests

--- webserver/test/frontend/JourneyImport.react-test.js
+++ webserver/test/frontend/JourneyImport.react-test.js
@@ -35,7 +35,7 @@ test('Valid journeys can be imported', () => {
   expect(_toJourneys(journeys)).to.not.throw(Error);
 })
 
-test('Invalid journeys cannot be imported', () => {
+test('Journeys with invalid properties cannot be imported', () => {
   const component = render.create(
     <JourneyImport/>
   );
@@ -46,6 +46,44 @@ test('Invalid journeys cannot be imported', () => {
   const journeys = [{
     objectID: 0,
     foo: {
+      lat: 2,
+      lng: 2
+    }
+  }]
+
+  expect(_toJourneys(journeys)).to.throw(Error);
+})
+
+test('Journeys with missing origin cannot be imported', () => {
+  const component = render.create(
+    <JourneyImport/>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  const journeys = [{
+    objectID: 0,
+    destination: {
+      lat: 2,
+      lng: 2
+    }
+  }]
+
+  expect(_toJourneys(journeys)).to.throw(Error);
+})
+
+test('Journeys with missing destination cannot be imported', () => {
+  const component = render.create(
+    <JourneyImport/>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  const journeys = [{
+    objectID: 0,
+    origin: {
       lat: 2,
       lng: 2
     }
